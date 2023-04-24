4456bef211b6a8de7ada7debee6f124f33233979
ABHISHEK KUMAR <cn4613@gmail.com>
2023-04-24 12:41:11 +0530

    Update

--- 2.Fundamentals of JavaScript/D.Fundamentals of JavaScript  Part B.md
+++ 2.Fundamentals of JavaScript/D.Fundamentals of JavaScript  Part B.md
@@ -67,4 +67,3 @@ switch(expression) {
     // code block
 }
 ```
-
--- 2.Fundamentals of JavaScript/F.Script B.js
+++ 2.Fundamentals of JavaScript/F.Script B.js
@@ -14,20 +14,46 @@ if (yourAge >= votingAge) {
 
 // if-else Statement
 
-const drivingAge = 18;
+const votingAge = 18;
 
 const yourAge = 17;
 
-if (yourAge >= drivingAge) {
+if (yourAge >= votingAge) {
 
     console.log(`You are eligible for driving licence.`)
 
 }  else {
 
-    const yearLeft = drivingAge - yourAge
+    const yearLeft = votingAge - yourAge
 
-    console.log(`You are not eligible for driving licence. Year left for eligibility is ${yearLeft}.`)
+    console.log(`You are not eligible for voting. Year left for eligibility is ${yearLeft} years.`)
 }
 
 // if-else if-else Statement
+
+const votingAge = 18;
+
+const contestingAge = 25;
+
+const yourAge = 17;
+
+if (yourAge >= votingAge & yourAge >= contestingAge) {
+
+    console.log("You are eligible for voting and contesting.")
+
+} else if (yourAge >= votingAge & yourAge < contestingAge) {
+
+    const yearLeft = contestingAge - yourAge;
+
+    console.log(`You are only eligible for voting not contesting.Year left for eligibility for the contesting is ${yearLeft} years.`)
+
+} else {
+
+    const yearLeftV = votingAge - yourAge
+    const yearLeftC = contestingAge - yourAge;
+
+    console.log(`You are neither eligible for the voting nor for contesting. Year left for eligibility for the voting is ${yearLeftV} years and for  contesting is ${yearLeftC} years.`)
+}
+
+
 
